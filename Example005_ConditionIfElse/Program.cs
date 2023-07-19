6a2732c17ea43f677851ad7ea8d5d4a81f8bb1e0
Mick <tambov.solid@bk.ru>
2023-07-20 00:46:36 +0600

    Example 005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write( "Ведите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "даша")
+{
+    Console.Write( "Ура!! это же Даша" );
+}
+else
+{
+    Console.Write( "Привет, " );
+    Console.WriteLine( username );
+}
